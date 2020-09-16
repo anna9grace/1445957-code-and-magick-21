0e334fd6624c8c9e03d9c9a97d70c428db54dd70
anna9grace <anna9grace@gmail.com>
2020-09-16 12:01:45 +0300

    добавляет в скрипт 'use strict'

--- js/script.js
+++ js/script.js
@@ -1,8 +1,10 @@
+'use strict';
+
 var fireballSize = 22;
 
 var getFireballSpeed = function (isToLeft) {
   return isToLeft ? 2 : 5;
-}
+};
 
 var wizardSpeed = 3;
 
@@ -10,12 +12,12 @@ var wizardWidth = 70;
 
 var getWizardHeight = function () {
   return wizardWidth * 1.337;
-}
+};
 
 var getWizardX = function (fieldWidth) {
   return (fieldWidth - wizardWidth) / 2;
-}
+};
 
 var getWizardY = function (fieldHeight) {
   return fieldHeight / 3;
-}
+};
